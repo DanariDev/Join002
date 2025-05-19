6ddc73cbc8f1bbb5674eeba3fea455b63d51b710
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2025-05-19 18:48:20 +0200

    contact page started with design 2

--- js/contacts.js
+++ js/contacts.js
@@ -1,27 +1,51 @@
-// Beispiel-Daten
+// Beispiel-Daten 
 let contacts = [
     "Anton Mayer", "Anja Schulz", "Benedikt Ziegler", "David Eisberg", 
-    "Eva Fischer", "Emanuel Mauer"
+    "Eva Fischer", "Emanuel Mauer", "Vorname Nachname1","Vorname Nachname2",
+    "Vorname Nachname3","Vorname Nachname4","Vorname Nachname5","Vorname Nachname6",
+    "Vorname Nachname7","Vorname Nachname8","Vorname Nachname9","Vorname Nachname10",
+    "Vorname Nachname11","Vorname Nachname12","Vorname Nachname13","Vorname Nachname14",
+    "Vorname Nachname15","Vorname Nachname16","Vorname Nachname17","Vorname Nachname18"
 ];
 
 let groupedContacts = [];
 
 function contactsList(){
+    sortContacts();
+    generateSortedContacts();
+}
+
+function sortContacts(){
     contacts.forEach(element => {
-        const firstLetter = element.charAt(0).toUpperCase();
+        let firstLetter = element.charAt(0).toUpperCase();
         if (!groupedContacts[firstLetter]) {
             groupedContacts[firstLetter] = [];
         }
         groupedContacts[firstLetter].push(element);
     });
+}
 
+function generateSortedContacts(){
     for (let indexaAlphabet = 0; indexaAlphabet < Object.keys(groupedContacts).length; indexaAlphabet++) {
-        document.getElementById('contacts-listID').innerHTML += `<div class="alphabet-div"><span>${Object.keys(groupedContacts)[indexaAlphabet]}</span><div class="separate-contacts-list"></div></div>`;
-        document.getElementById('contacts-listID').innerHTML += `<div class="group-list" id="group-list${indexaAlphabet}ID"></div>`
+        document.getElementById('contacts-listID').innerHTML += createAlphabetDiv(indexaAlphabet);
+        document.getElementById('contacts-listID').innerHTML += createGroupList(indexaAlphabet);
 
         for (let indexContacs = 0; indexContacs < Object.values(groupedContacts)[indexaAlphabet].length; indexContacs++) {
-            document.getElementById(`group-list${indexaAlphabet}ID`).innerHTML += `<div class="img-name-email-div"><div class="img-div" id="img-div${indexaAlphabet}${indexContacs}ID">${Object.values(groupedContacts)[indexaAlphabet][indexContacs].slice(0,1) + Object.values(groupedContacts)[indexaAlphabet][indexContacs].split(' ')[1].slice(0,1)}</div><div class="name-email-div"><span>${Object.values(groupedContacts)[indexaAlphabet][indexContacs]}</span><span class="email-span">E-mail<span/></div>`;
+            document.getElementById(`group-list${indexaAlphabet}ID`).innerHTML += createImgNameEmailDiv(indexaAlphabet, indexContacs)
             document.getElementById(`img-div${indexaAlphabet}${indexContacs}ID`).style.backgroundColor = `hsl(${Math.random()* 360}, ${(Math.random() * 20) + 50}%, ${(Math.random() * 20) + 50}%`;
         }
     }
 }
+
+function createAlphabetDiv(indexaAlphabet){
+    return `<div class="alphabet-div"><span>${Object.keys(groupedContacts)[indexaAlphabet]}</span><div class="separate-contacts-list"></div></div>`;
+}
+
+function createGroupList(indexaAlphabet){
+    return `<div class="group-list" id="group-list${indexaAlphabet}ID"></div>`
+}
+
+function createImgNameEmailDiv(indexaAlphabet, indexContacs){
+    return `<div class="img-name-email-div"><div class="img-div" id="img-div${indexaAlphabet}${indexContacs}ID">${Object.values(groupedContacts)[indexaAlphabet][indexContacs].slice(0,1) + Object.values(groupedContacts)[indexaAlphabet][indexContacs].split(' ')[1].slice(0,1)}</div><div class="name-email-div"><span>${Object.values(groupedContacts)[indexaAlphabet][indexContacs]}</span><span class="email-span">E-mail<span/></div>`;
+}
+
