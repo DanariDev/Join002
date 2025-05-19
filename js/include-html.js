77cb8b864e5cbe8330179263178fdf6cbd27fa35
Stefan <zeroger.lp@gmail.com>
2025-05-20 01:34:56 +0200

    css anpassungens

--- js/include-html.js
+++ js/include-html.js
@@ -1,19 +1,18 @@
 function includeHTML() {
-    const elements = document.querySelectorAll('[include-html]');
-    elements.forEach(el => {
-      const file = el.getAttribute('include-html');
-      fetch(file)
-        .then(response => response.text())
-        .then(data => {
-          el.innerHTML = data;
-          el.removeAttribute('include-html');
-          includeHTML(); // Für verschachtelte Includes
-        })
-        .catch(err => {
-          el.innerHTML = "Sidebar konnte nicht geladen werden.";
-          console.error(err);
-        });
-    });
-  }
-  window.addEventListener('DOMContentLoaded', includeHTML);
-  
+  const elements = document.querySelectorAll('[include-html]');
+  elements.forEach(el => {
+    const file = el.getAttribute('include-html');
+    fetch(file)
+      .then(response => response.text())
+      .then(data => {
+        el.innerHTML = data;
+        el.removeAttribute('include-html');
+        includeHTML(); // Für verschachtelte Includes
+      })
+      .catch(err => {
+        el.innerHTML = "Sidebar konnte nicht geladen werden.";
+        console.error(err);
+      });
+  });
+}
+window.addEventListener('DOMContentLoaded', includeHTML);
