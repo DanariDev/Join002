b5be5dca506a9cdb2be11914c66cc0c216ab54b2
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2025-05-24 13:53:57 +0200

    test2

---
+++ js/add-edit-contact.js
@@ -0,0 +1,10 @@
+function addContactOpenClose(){
+    document.getElementById('add-contact-divID').classList.toggle('display-none');
+    document.getElementById('add-edit-bodyID').classList.toggle('display-none');
+    
+}
+
+function editContactOpenClose(){
+    document.getElementById('edit-contact-divID').classList.toggle('display-none');
+    document.getElementById('add-edit-bodyID').classList.toggle('display-none');
+}
