ad76892d6fea2e3498fa367bd5a7e958dc80c309
Stefan <zeroger.lp@gmail.com>
2025-06-03 20:15:11 +0200

    Contacts angepasst, hier werden nun die Identischen Farben für die initialien wie im Board verwendet

--- js/contacts.js
+++ js/contacts.js
@@ -3,11 +3,25 @@ let contacts = [];
 let groupedContacts = [];
 
 
+function getColorForName(name) {
+    const colors = [
+        '#FF5733', '#33B5FF', '#33FF99', '#FF33EC', '#ffcb20',
+        '#9D33FF', '#33FFDA', '#FF8C33', '#3385FF', '#FF3333'
+    ];
+    let hash = 0;
+    for (let i = 0; i < name.length; i++) {
+        hash = name.charCodeAt(i) + ((hash << 5) - hash);
+    };
+    return colors[Math.abs(hash) % colors.length];
+};
+
+
 async function initContactsList() {
     await onLoadContacts();
     sortContacts();
     generateSortedContacts();
-}
+};
+
 
 async function onLoadContacts() {
     document.getElementById('contacts-listID').innerHTML = "";
@@ -23,14 +37,16 @@ async function onLoadContacts() {
                 id: ContactKeysArray[index],
                 contact: contactResponse[ContactKeysArray[index]],
             }
-        )
-    }
-}
+        );
+    };
+};
+
 
 async function getAllContacts(path) {
     let response = await fetch(BASE_URL + path + ".json");
     return responseToJson = await response.json();
-}
+};
+
 
 function sortContacts() {
     contacts.sort((a, b) => a.contact.name.localeCompare(b.contact.name));
@@ -38,10 +54,11 @@ function sortContacts() {
         let firstLetter = element.contact.name.charAt(0).toUpperCase();
         if (!groupedContacts[firstLetter]) {
             groupedContacts[firstLetter] = [];
-        }
+        };
         groupedContacts[firstLetter].push(element);
     });
-}
+};
+
 
 function generateSortedContacts() {
     for (let indexaAlphabet = 0; indexaAlphabet < Object.keys(groupedContacts).length; indexaAlphabet++) {
@@ -50,44 +67,41 @@ function generateSortedContacts() {
 
         for (let indexContacs = 0; indexContacs < Object.values(groupedContacts)[indexaAlphabet].length; indexContacs++) {
             document.getElementById(`group-list${indexaAlphabet}ID`).innerHTML += createImgNameEmailDiv(indexaAlphabet, indexContacs)
-            document.getElementById(`img-div${indexaAlphabet}${indexContacs}ID`).style.backgroundColor = Object.values(groupedContacts)[indexaAlphabet][indexContacs].contact.iconBackgroundColor; //`hsl(${Math.random()* 360}, ${(Math.random() * 20) + 50}%, ${(Math.random() * 20) + 50}%`;
-        }
-    }
-}
+        };
+    };
+};
+
 
 function createAlphabetDiv(indexaAlphabet) {
     return `<div class="alphabet-div"><span>${Object.keys(groupedContacts)[indexaAlphabet]}</span><div class="separate-contacts-list"></div></div>`;
-}
+};
+
 
 function createGroupList(indexaAlphabet) {
     return `<div class="group-list" id="group-list${indexaAlphabet}ID"></div>`
-}
+};
 
-//function createImgNameEmailDiv(indexaAlphabet, indexContacs){
-//    return `<div class="img-name-email-div" id="img-name-email-div${indexaAlphabet}${indexContacs}ID" onclick="contactDetailsLoad('${indexaAlphabet}${indexContacs}','${indexaAlphabet}','${indexContacs}'), toContactDetails()"><div class="img-div" id="img-div${indexaAlphabet}${indexContacs}ID">${Object.values(groupedContacts)[indexaAlphabet][indexContacs].contact.initials}</div><div class="name-email-div"><span id=name${indexaAlphabet}${indexContacs}ID>${Object.values(groupedContacts)[indexaAlphabet][indexContacs].contact.name}</span><span class="email-span" id="email${indexaAlphabet}${indexContacs}ID">${Object.values(groupedContacts)[indexaAlphabet][indexContacs].contact.email}<span/></div>`;
-//}
 
-// Vorschlag für diese function mit geringerer Fehleranfälligkeit und übersichtlicherer Struktur //
 function createImgNameEmailDiv(indexaAlphabet, indexContacs) {
     const contact = Object.values(groupedContacts)[indexaAlphabet][indexContacs].contact;
     const initials = contact.initials;
     const name = contact.name;
     const email = contact.email;
+    const bgColor = getColorForName(name);
     return `
     <div class="img-name-email-div" id="img-name-email-div${indexaAlphabet}${indexContacs}ID" 
          onclick="contactDetailsLoad('${indexaAlphabet}${indexContacs}','${indexaAlphabet}','${indexContacs}'), toContactDetails()">
-        <div class="img-div" id="img-div${indexaAlphabet}${indexContacs}ID">${initials}</div>
+        <div class="img-div" id="img-div${indexaAlphabet}${indexContacs}ID" style="background-color: ${bgColor};">${initials}</div>
         <div class="name-email-div">
             <span id="name${indexaAlphabet}${indexContacs}ID">${name}</span>
             <span class="email-span" id="email${indexaAlphabet}${indexContacs}ID">${email}</span>
         </div>
     </div>`;
-}
+};
+
 
 function contactDetailsLoad(idNumber, indexaAlphabet, indexContacs) {
     document.getElementById('contacts-details-contentsID').classList.add('display-flex');
-    document.getElementById('img-details-divID').style.backgroundColor = Object.values(groupedContacts)[indexaAlphabet][indexContacs].contact.iconBackgroundColor;
-
     document.getElementById('img-details-divID').innerHTML = Object.values(groupedContacts)[indexaAlphabet][indexContacs].contact.initials;
     document.getElementById('details-nameID').innerHTML = Object.values(groupedContacts)[indexaAlphabet][indexContacs].contact.name;
     document.getElementById('details-emailID').innerHTML = Object.values(groupedContacts)[indexaAlphabet][indexContacs].contact.email;
@@ -96,10 +110,9 @@ function contactDetailsLoad(idNumber, indexaAlphabet, indexContacs) {
     for (let index = 0; index < document.getElementsByClassName('img-name-email-div').length; index++) {
         document.getElementsByClassName('img-name-email-div')[index].style = "";
     }
-    document.getElementById(`img-name-email-div${idNumber}ID`).style.backgroundColor = '#2a3647';
-    document.getElementById(`img-name-email-div${idNumber}ID`).style.color = 'white';
 
-}
+};
+
 
 function backToContactsList() {
     document.getElementById('contact-details-divID').classList.remove('display-flex');
@@ -107,8 +120,9 @@ function backToContactsList() {
         element.style = "";
     });
     document.getElementById('contacts-details-contentsID').classList.remove('display-flex');
-}
+};
+
 
 function toContactDetails() {
     document.getElementById('contact-details-divID').classList.add('display-flex');
-}
+};
