667a044e9889a62b2ef3c09403fd4512d3a00b94
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2025-06-10 14:38:30 +0200

    fix: sidebar unregistered adapted 3

--- js/changeSidebar.js
+++ js/changeSidebar.js
@@ -1,12 +1,24 @@
-function init(){
+function init() {
+    const { navLinkHideAllClass, topbarIcons, navLinkUnregistered, sidebarFooterLinks, sidebarFooter } = initDomElements();
+
     if(localStorage.getItem('unregistered') == 'true'){
-        document.querySelectorAll(".nav-link-hide").forEach((element) => {
+        navLinkHideAllClass.forEach((element) => {
             element.classList.add('d-none');
         });
-        document.getElementById('topbar-iconsID').classList.add('d-none')
-        document.getElementById('nav-link-unregisteredID').classList.remove('d-none');
 
-        document.getElementById('sidebar-footer-linksID').classList.add('unregistered-sidebar')
-        document.getElementById('sidebar-footer').classList.add('d-flex');
+        topbarIcons.classList.add('d-none')
+        navLinkUnregistered.classList.remove('d-none');
+        sidebarFooterLinks.classList.add('unregistered-sidebar')
+        sidebarFooter.classList.add('d-flex');
     }
+}
+
+function initDomElements(){
+    const navLinkHideAllClass = document.querySelectorAll(".nav-link-hide");
+    const topbarIcons = document.getElementById('topbar-iconsID');
+    const navLinkUnregistered = document.getElementById('nav-link-unregisteredID');
+    const sidebarFooterLinks = document.getElementById('sidebar-footer-linksID');
+    const sidebarFooter = document.getElementById('sidebar-footer');
+
+    return {navLinkHideAllClass, topbarIcons, navLinkUnregistered, sidebarFooterLinks, sidebarFooter}
 }
