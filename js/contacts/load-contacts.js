866af5ec84546f89d950e0282897f65c6fa4d2cd
Braunerbaerr <danielkwiatkowski@gmx.de>
2025-07-23 11:47:45 +0200

    contacts werden geladen aus Database

--- js/contacts/load-contacts.js
+++ js/contacts/load-contacts.js
@@ -1,18 +1,12 @@
-import {
-  getDatabase,
-  ref,
-  get
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
-import { app } from "../firebase/firebase-init.js";
-import { getInitials, getRandomColor } from "./contact-style.js";
-
-const db = getDatabase(app);
+import { db } from '../firebase/firebase-init.js';
+import { ref, get } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js';
 
 export function initContactsList() {
-  const refContacts = ref(db, 'contacts');
-  get(refContacts).then(snapshot => {
+  const contactsRef = ref(db, 'contacts');
+  get(contactsRef).then(snapshot => {
     if (!snapshot.exists()) return;
-    renderContacts(Object.values(snapshot.val()));
+    const contacts = Object.values(snapshot.val());
+    renderContacts(contacts);
   });
 }
 
@@ -20,20 +14,25 @@ function renderContacts(contacts) {
   const wrapper = document.querySelector('#contacts-list-wrapper');
   if (!wrapper) return;
   wrapper.innerHTML = '';
-  contacts.forEach(c => wrapper.appendChild(createContactHTML(c)));
+  contacts.forEach(contact => wrapper.append(createContactHTML(contact)));
 }
 
 function createContactHTML(contact) {
-  const initials = getInitials(contact.name);
-  const color = getRandomColor(contact.name);
   const div = document.createElement('div');
   div.className = 'list-contact-wrapper';
-  div.dataset.email = contact.email;
   div.innerHTML = `
-    <div class="initial-icon" style="background-color:${color}">${initials}</div>
+    <div class="initial-icon">${getInitials(contact.name)}</div>
     <div class="list-contact-information">
       <span class="list-name">${contact.name}</span>
       <span class="list-email">${contact.email}</span>
     </div>`;
   return div;
+}
+
+function getInitials(name) {
+  return name
+    .split(' ')
+    .map(w => w[0]?.toUpperCase())
+    .join('')
+    .slice(0, 2);
 }
