de51c82c85adc0f93ed20d736fd8f4fae6d545a9
Braunerbaerr <danielkwiatkowski@gmx.de>
2025-07-30 14:49:24 +0200

    contacts eingefügt

--- js/board/main.js
+++ js/board/main.js
@@ -2,12 +2,12 @@ import { loadTasks } from "./load-tasks.js";
 import { initOverlay } from "./overlay.js";
 import { initSearch } from "./search.js";
 import { deleteTask } from "./delete-task.js";
-import { openEditTaskOverlay } from "./edit-task.js";   
-import { initEditTaskForm } from "./edit-task-form.js";  
+import { openEditTaskOverlay } from "./edit-task.js";
+import { initEditTaskForm } from "./edit-task-form.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   loadTasks();
   initOverlay();
   initSearch();
-  initEditTaskForm(); 
+  initEditTaskForm();
 });
