989a4e038c47ef41eea4530d37d8e55cbfabc910
Braunerbaerr <danielkwiatkowski@gmx.de>
2026-02-06 21:13:24 +0100

    Update config.php

--- api/config.php
+++ api/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_NAME', 'join_app');
-define('DB_USER', 'join_user');
+define('DB_USER', 'join_app');
 define('DB_PASS', 'Goldhammster06!');
 
 // Session settings
