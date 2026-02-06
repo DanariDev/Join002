7b05eb27ae2cf615d2cf4dbb35e9f323a7e3287a
Braunerbaerr <danielkwiatkowski@gmx.de>
2026-02-06 21:05:20 +0100

    Update config.php

--- api/config.php
+++ api/config.php
@@ -4,7 +4,7 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_NAME', 'join_app');
 define('DB_USER', 'join_user');
-define('DB_PASS', 'twitter0sprotte*DANNER');
+define('DB_PASS', 'Reinerswurst1996!');
 
 // Session settings
 define('SESSION_TTL_HOURS', 24);
